13513c3cff8b2809eda6eff035d923d8790fe5c7
Spencer <37875712+Penjat@users.noreply.github.com>
2021-06-28 21:41:38 -0700

    Initial Commit

---
+++ MazeMaker/MazeMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeMakerApp.swift
+//  MazeMaker
+//
+//  Created by Spencer Symington on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct MazeMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
