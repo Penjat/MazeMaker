044cdae1eaafb390033a969a2d8e848e9026a5b4
Spencer <37875712+Penjat@users.noreply.github.com>
2021-07-11 08:11:34 -0700

    Correct subdivisions.

--- MazeMaker/Views/PolarMazeView.swift
+++ MazeMaker/Views/PolarMazeView.swift
@@ -17,12 +17,15 @@ struct PolarMazeView: View {
     func createCells() -> [PolarRow] {
         var rows = [PolarRow]()
         
+        var numberCells =  8
         for column in 0..<10 {
             //            let innerRadians = CGFloat(col)*ringHeight
             let col = CGFloat(column)
             let rowArea = CGFloat.pi*CGFloat(ringHeight*col)*CGFloat(ringHeight*col) - CGFloat.pi*CGFloat(ringHeight*(col-1))*CGFloat(ringHeight*(col-1))
             let idealCellArea = ringHeight*ringHeight
-            let numberCells =  4 + Int(rowArea / idealCellArea)
+            if rowArea/CGFloat(numberCells) > idealCellArea*2 {
+                numberCells = numberCells*2
+            }
             print("column is: \(column) - \(numberCells) cells")
             var cells = [PolarCell]()
             for row in 0..<numberCells {
