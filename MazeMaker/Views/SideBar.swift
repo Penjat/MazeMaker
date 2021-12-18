2ceb7fb6f9a462dadd1c5f61a003a819435daec7
Spencer <37875712+Penjat@users.noreply.github.com>
2021-12-18 14:14:46 -0330

    Tunnel length (#14)
    * Fixed prims tunnels.
    
    * tidy.

--- MazeMaker/Views/SideBar.swift
+++ MazeMaker/Views/SideBar.swift
@@ -53,7 +53,8 @@ struct SideBar: View {
                         displaySettings.mazeProvider.clearAll()
                         let prims = PrimsMazeGenerator()
                         prims.activeCells.append(displaySettings.mazeProvider.randomCell()!)
-                        prims.simplifiedFindPaths(mazeProvider: displaySettings.mazeProvider)
+//                        prims.simplifiedFindPaths(mazeProvider: displaySettings.mazeProvider)
+                        prims.findPathsCycling(mazeProvider: displaySettings.mazeProvider)
                         displaySettings.mazeProvider.clearData()
                         
                         let ds = DijkstraService()
---
+++ PolyMazeMaker/MazeWall.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+class MazeWall {
+    var isOpen: Bool = true
+    
+}
